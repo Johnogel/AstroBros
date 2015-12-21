2d932a2116e54768a9e42d8e972a4b66ae273358
johno-gel <johno-gel@johno-gel-P5N-T-DELUXE>
2015-12-21 16:28:25 -0600

    Beefed up some classes with stats and positions and stuff like that.

--- AstroBros/core/src/com/johnogel/astrobros/AstroBro.java
+++ AstroBros/core/src/com/johnogel/astrobros/AstroBro.java
@@ -5,7 +5,13 @@
  */
 package com.johnogel.astrobros;
 
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
 
 /**
@@ -15,9 +21,31 @@ import com.badlogic.gdx.physics.box2d.World;
 public class AstroBro implements GameObject{
 private World world;
 private OrthographicCamera camera;
+private Sound[] sound;
+private Body astro_body;
     public AstroBro(World world, OrthographicCamera camera){
         this.world = world;
         this.camera = camera;
+        sound = new Sound[20];
+        
+        BodyDef circle_def = new BodyDef();
+        circle_def.type = BodyType.DynamicBody;
+        float x = (float) (camera.viewportWidth*Math.random());
+        float y = (float) (camera.viewportHeight*Math.random());
+        circle_def.position.set(x,y);
+        
+        astro_body = world.createBody(circle_def);
+        CircleShape circle_shape = new CircleShape();
+        circle_shape.setRadius(3f);
+        
+        FixtureDef circle_fixture = new FixtureDef();
+        circle_fixture.shape = circle_shape;
+        circle_fixture.density = .5f;
+        circle_fixture.friction = .8f;
+        circle_fixture.restitution = .5f;
+        
+        astro_body.createFixture(circle_fixture);
+        
     }
     
     @Override
--- AstroBros/core/src/com/johnogel/astrobros/GameManager.java
+++ AstroBros/core/src/com/johnogel/astrobros/GameManager.java
@@ -5,10 +5,14 @@
  */
 package com.johnogel.astrobros;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.FPSLogger;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
+import java.util.ArrayList;
 
 /**
  *
@@ -16,24 +20,49 @@ import com.badlogic.gdx.physics.box2d.World;
  */
 public class GameManager implements Controller{
 World world; 
-Box2DDebugRenderer renderer;
-FPSLogger logger;
+private Box2DDebugRenderer renderer;
+private FPSLogger logger;
+private int width, height;
+private OrthographicCamera camera;
+private ArrayList<GameObject> game_objects;
     public GameManager(){
+        width = Gdx.graphics.getWidth()/3;
+        height = Gdx.graphics.getHeight()/3;
         world = new World(new Vector2(0,0), false);
         renderer = new Box2DDebugRenderer();
         logger = new FPSLogger();
+        camera = new OrthographicCamera(width, height);
+        
+        game_objects = new ArrayList();
+        for (int i = 0; i < 15; i++){
+            game_objects.add(new AstroBro(world, camera));
+        }
+        
         
         
     }
     
     @Override
     public void render(){
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        
+    
+        
+        renderer.render(world, camera.combined);
+        
         
     }
 
     @Override
     public void update() {
         
+    }
+    
+    private void renderGameObjects(){
+        for (GameObject o : game_objects){
+            o.render();
+        }
     }
     
 }
